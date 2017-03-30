82b37166cfc58a283ed5964ad62d1cad54592b42
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-03-30 14:43:28 +0200

    manage.sh: use built-in 'venv' on Python 3.3 and later

--- manage.sh
+++ manage.sh
@@ -16,13 +16,22 @@ sys.stdout.write(("%s-%s.%s" % (
 )).lower())
 EOF
 )
+virtualenv_module=$($PYTHON <<EOF
+import sys, platform
+sys.stdout.write(
+  'virtualenv'
+  if platform.python_version_tuple()[:2] < ('3', '3')
+  else 'venv'
+)
+EOF
+)
 
 os=$(uname -s | tr A-Z a-z)
 env=/tmp/pyenv-$(basename $dir)-$os-$pyvers
 
 if ! test -d "$env"
 then
-   $PYTHON -m virtualenv $env
+   $PYTHON -m $virtualenv_module $env
 fi
 
 . $env/bin/activate
