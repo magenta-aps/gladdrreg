d6d41a838446fdd1b8e2fa435d8698bb67df9116
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-05-31 17:13:52 +0200

    Remove user-visible references to Django

--- addrreg/admin.py
+++ addrreg/admin.py
@@ -1,5 +1,17 @@
-from __future__ import absolute_import, unicode_literals, print_function
+# -*- mode: python; coding: utf-8 -*-
 
 from django.contrib import admin
 
-# Register your models here.
+from django.utils.translation import ugettext_lazy as _
+
+from . import apps
+
+
+# Text to put at the end of each page's <title>.
+admin.site.site_title = apps.AddrRegConfig.verbose_name
+
+# Text to put in each page's <h1> (and above login form).
+admin.site.site_header = apps.AddrRegConfig.verbose_name
+
+# Text to put at the top of the admin index page.
+admin.site.index_title = _('Administration Overview')
