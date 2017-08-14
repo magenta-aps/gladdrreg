abcedacfe58acfbebc5008cd01f0ecd1c180d571
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2017-08-14 11:33:50 +0200

    customise admin interface
    - hide groups & permissions
    - show staff & superuser checkboxes at creation

--- addrreg/admin.py
+++ addrreg/admin.py
@@ -1,10 +1,13 @@
 # -*- mode: python; coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import models as auth_models
 
 from django.utils.translation import ugettext_lazy as _
 
 from . import apps
+from . import forms
 
 
 # Restrict available filters only to things that exist.
@@ -25,3 +28,26 @@ admin.site.index_title = _('Administration Overview')
 
 # Link to documentation
 admin.site.site_url = apps.AddrRegConfig.documentation
+
+
+class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': (
+            'is_active', 'is_staff', 'is_superuser',
+        )}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2',
+                       'is_staff', 'is_superuser'),
+        }),
+    )
+
+
+admin.site.unregister(auth_models.User)
+admin.site.register(auth_models.User, UserAdmin)
